5fa64417b926e74b9f209587589c6d8c463743e8
Osama <osama.hassan.oh@gmail.com>
2026-02-01 23:57:56 +0300

    Initial Commit

---
+++ FeedAppCleanArchitecture/FeedAppCleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedAppCleanArchitectureApp.swift
+//  FeedAppCleanArchitecture
+//
+//  Created by Osama AlMekhlafi on 01/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct FeedAppCleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
