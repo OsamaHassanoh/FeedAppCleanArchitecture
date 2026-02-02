f2ce894391a19939b5eb3a7004b790d0ecea2dfe
Osama <osama.hassan.oh@gmail.com>
2026-02-02 13:09:32 +0300

    structured application

---
+++ FeedAppCleanArchitecture/Core/DI/AppDIContainer.swift
@@ -0,0 +1,52 @@
+//
+//  DIContainer.swift
+//  FeedApplication
+//
+//  Created by Osama AlMekhlafi on 31/01/2026.
+//
+//
+//final class DIContainer {
+//    static let shared = DIContainer()
+//    
+//    private init() {}
+//    
+//    // MARK: - Network Layer
+//    private lazy var networkService: Network = {
+//        NetworkServiceImpl.shared
+//    }()
+//    
+//    // MARK: - Repositories
+//    private lazy var feedRepository: FeedRepoProtocol = {
+//        FeedRepository(network: networkService)
+//    }()
+//    
+//    // MARK: - Use Cases
+//    func makeFetchFeedsUseCase() -> FetchFeedsUseCaseProtocol {
+//        FetchFeedsUseCase(repository: feedRepository)
+//    }
+//    
+//    // MARK: - ViewModels
+//    func makeFeedViewModel() -> FeedViewModel {
+//        FeedViewModel(fetchFeedsUseCase: makeFetchFeedsUseCase())
+//    }
+//}
+
+
+// AppDIContainer.swift
+final class AppDIContainer {
+    static let shared = AppDIContainer()
+    
+    // MARK: - Shared Dependencies
+    private lazy var networkService: Network = {
+        NetworkServiceImpl.shared
+    }()
+    
+    // MARK: - Feature Containers
+    lazy var feedContainer: FeedDIContainer = {
+        FeedDIContainer(networkService: networkService)
+    }()
+    
+
+    
+    private init() {}
+}
---
+++ FeedAppCleanArchitecture/Features/Feed/FeedDIContainer.swift
@@ -0,0 +1,8 @@
+//
+//  FeedDIContainer.swift
+//  FeedAppCleanArchitecture
+//
+//  Created by Osama AlMekhlafi on 02/02/2026.
+//
+
+import Foundation
